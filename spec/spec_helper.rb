0ee9e5091679387c292f41b877858c4937e0c47b
Daniel Martin <etd-github@nomejortu.com>
2015-06-01 12:16:42 +0200

    RSpec - add :filter configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,4 +11,8 @@ RSpec.configure do |config|
   config.color = true
   # Use the specified formatter
   config.formatter = :documentation
+
+  # Filter which specs to run
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 end
