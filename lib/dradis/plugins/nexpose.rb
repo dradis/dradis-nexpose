5a7d014d24f9a4ed8f459761cb78b7a1cc1556e8
Daniel Martin <etd-github@nomejortu.com>
2016-03-14 12:50:29 +0100

    We no longer need a Meta module

--- lib/dradis/plugins/nexpose.rb
+++ lib/dradis/plugins/nexpose.rb
@@ -9,21 +9,3 @@ require 'dradis/plugins/nexpose/engine'
 require 'dradis/plugins/nexpose/field_processor'
 require 'dradis/plugins/nexpose/importer'
 require 'dradis/plugins/nexpose/version'
-
-
-module Dradis
-  module Plugins
-    module Nexpose
-      # This is required while we transition the Upload Manager to use
-      # Dradis::Plugins
-      module Meta
-        NAME = "NeXpose XML file upload"
-        EXPECTS = "NeXpose Simple or Full XML format."
-
-        module VERSION
-          include Dradis::Plugins::Nexpose::VERSION
-        end
-      end
-    end
-  end
-end
