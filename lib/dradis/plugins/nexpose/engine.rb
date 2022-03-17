4fed153877699a5b7fd1e8635920f505a8d43381
Brian Pearce <brian.o.pearce@gmail.com>
2022-03-17 15:20:42 +0100

    Register templates locally

--- lib/dradis/plugins/nexpose/engine.rb
+++ lib/dradis/plugins/nexpose/engine.rb
@@ -5,5 +5,9 @@ module Dradis::Plugins::Nexpose
     include ::Dradis::Plugins::Base
     description 'Processes Nexpose XML format'
     provides :upload
+
+    def self.template_names
+      { module_parent => { evidence: 'full_evidence', issue: 'full_vulnerability' } }
+    end
   end
 end
