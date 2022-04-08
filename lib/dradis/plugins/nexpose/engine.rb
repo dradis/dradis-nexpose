46026b84dcacfb8f0b32d388c567d8ba0c8455f9
Brian Pearce <brian.o.pearce@gmail.com>
2022-04-08 18:35:58 +0200

    move templates to importer

--- lib/dradis/plugins/nexpose/engine.rb
+++ lib/dradis/plugins/nexpose/engine.rb
@@ -5,9 +5,5 @@ module Dradis::Plugins::Nexpose
     include ::Dradis::Plugins::Base
     description 'Processes Nexpose XML format'
     provides :upload
-
-    def self.template_names
-      { module_parent => { evidence: 'full_evidence', issue: 'full_vulnerability' } }
-    end
   end
 end
