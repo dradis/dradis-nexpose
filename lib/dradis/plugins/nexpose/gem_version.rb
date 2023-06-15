4c1ea27ecc911dbb3bafa77ffa404c059019fc86
Aaron Manaloto <mail@aapomm.io>
2023-06-15 16:53:16 +0800

    Bump version and update changelog

--- lib/dradis/plugins/nexpose/gem_version.rb
+++ lib/dradis/plugins/nexpose/gem_version.rb
@@ -8,11 +8,11 @@ module Dradis
 
       module VERSION
         MAJOR = 4
-        MINOR = 9
+        MINOR = 10
         TINY = 0
         PRE = nil
 
-        STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
+        STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
       end
     end
   end
