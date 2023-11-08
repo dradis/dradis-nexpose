39c06974da657a66f4fdd76e6e3d2bfcd26a8dff
Caitlin <caitlinhenry.ss@gmail.com>
2023-11-08 12:41:23 -0500

    lint

--- spec/nexpose_upload_spec.rb
+++ spec/nexpose_upload_spec.rb
@@ -38,20 +38,20 @@ describe 'Nexpose upload plugin' do
     end
   end
 
-  describe "Importer: Simple" do
-    it "creates nodes, issues, notes and an evidences as needed" do
+  describe 'Importer: Simple' do
+    it 'creates nodes, issues, notes and an evidences as needed' do
 
       expect(@content_service).to receive(:create_node).with(hash_including label: '1.1.1.1', type: :host).once
 
       expect(@content_service).to receive(:create_note) do |args|
-        expect(args[:text]).to include("Host Description : Linux 2.6.9-89.ELsmp")
-        expect(args[:text]).to include("Scanner Fingerprint certainty : 0.80")
-        expect(args[:node].label).to eq("1.1.1.1")
+        expect(args[:text]).to include('Host Description : Linux 2.6.9-89.ELsmp')
+        expect(args[:text]).to include('Scanner Fingerprint certainty : 0.80')
+        expect(args[:node].label).to eq('1.1.1.1')
       end.once
 
       expect(@content_service).to receive(:create_node) do |args|
         expect(args[:label]).to eq('Generic Findings')
-        expect(args[:parent].label).to eq("1.1.1.1")
+        expect(args[:parent].label).to eq('1.1.1.1')
         OpenStruct.new(args)
       end.once
 
@@ -64,13 +64,13 @@ describe 'Nexpose upload plugin' do
       expect(@content_service).to receive(:create_note) do |args|
         expect(args[:text]).to include("#[Id]#\nntpd-crypto")
         expect(args[:text]).to include("#[host]#\n1.1.1.1")
-        expect(args[:node].label).to eq("udp-000")
+        expect(args[:node].label).to eq('udp-000')
       end.once
 
       expect(@content_service).to receive(:create_note) do |args|
         expect(args[:text]).to include("#[Id]#\nntp-clock-radio")
         expect(args[:text]).to include("#[host]#\n1.1.1.1")
-        expect(args[:node].label).to eq("udp-000")
+        expect(args[:node].label).to eq('udp-000')
       end.once
 
       @importer.import(file: 'spec/fixtures/files/simple.xml')
@@ -82,7 +82,7 @@ describe 'Nexpose upload plugin' do
       expect(@content_service).to receive(:create_node).with(hash_including label: "Nexpose Scan Summary").once
       expect(@content_service).to receive(:create_note) do |args|
         expect(args[:text]).to include("#[Title]#\nUSDA_Internal (4)")
-        expect(args[:node].label).to eq("Nexpose Scan Summary")
+        expect(args[:node].label).to eq('Nexpose Scan Summary')
       end.once
 
       expect(@content_service).to receive(:create_node).with(
@@ -91,41 +91,41 @@ describe 'Nexpose upload plugin' do
 
       expect(@content_service).to receive(:create_note) do |args|
         expect(args[:text]).to include("#[Title]#\n1.1.1.1")
-        expect(args[:node].label).to eq("1.1.1.1")
+        expect(args[:node].label).to eq('1.1.1.1')
       end.once
 
       expect(@content_service).to receive(:create_note) do |args|
         expect(args[:text]).to include("#[Title]#\nService name: NTP")
-        expect(args[:node].label).to eq("1.1.1.1")
+        expect(args[:node].label).to eq('1.1.1.1')
       end.once
 
       expect(@content_service).to receive(:create_note) do |args|
         expect(args[:text]).to include("#[Title]#\nService name: SNMP")
-        expect(args[:node].label).to eq("1.1.1.1")
+        expect(args[:node].label).to eq('1.1.1.1')
       end.once
 
       expect(@content_service).to receive(:create_issue) do |args|
         expect(args[:text]).to include("#[Title]#\nApache HTTPD: error responses can expose cookies (CVE-2012-0053)")
-        expect(args[:id]).to eq("ntp-clock-variables-disclosure")
+        expect(args[:id]).to eq('ntp-clock-variables-disclosure')
         OpenStruct.new(args)
       end.once
 
       expect(@content_service).to receive(:create_issue) do |args|
         expect(args[:text]).to include("#[Title]#\nApache HTTPD: ETag Inode Information Leakage (CVE-2003-1418)")
-        expect(args[:id]).to eq("test-02")
+        expect(args[:id]).to eq('test-02')
         OpenStruct.new(args)
       end.once
 
       expect(@content_service).to receive(:create_evidence) do |args|
         expect(args[:content]).to include("#[ID]#\nntp-clock-variables-disclosure\n\n")
-        expect(args[:issue].id).to eq("ntp-clock-variables-disclosure")
-        expect(args[:node].label).to eq("1.1.1.1")
+        expect(args[:issue].id).to eq('ntp-clock-variables-disclosure')
+        expect(args[:node].label).to eq('1.1.1.1')
       end.once
 
       expect(@content_service).to receive(:create_evidence) do |args|
         expect(args[:content]).to include("#[ID]#\ntest-02\n\n")
-        expect(args[:issue].id).to eq("test-02")
-        expect(args[:node].label).to eq("1.1.1.1")
+        expect(args[:issue].id).to eq('test-02')
+        expect(args[:node].label).to eq('1.1.1.1')
       end.once
 
       allow_any_instance_of(OpenStruct).to receive(:respond_to?).with(:properties).and_return(true)
@@ -139,9 +139,9 @@ describe 'Nexpose upload plugin' do
       @importer.import(file: 'spec/fixtures/files/full.xml')
     end
 
-    it "wraps ciphers inside ssl issues in code blocks" do
+    it 'wraps ciphers inside ssl issues in code blocks' do
       expect(@content_service).to receive(:create_issue) do |args|
-        expect(args[:text]).to include("bc. ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256")
+        expect(args[:text]).to include('bc. ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256')
         OpenStruct.new(args)
       end.once
 
@@ -149,7 +149,7 @@ describe 'Nexpose upload plugin' do
     end
 
     # Regression test for github.com/dradis/dradis-nexpose/issues/1
-    it "populates solutions regardless of if they are wrapped in paragraphs or lists" do
+    it 'populates solutions regardless of if they are wrapped in paragraphs or lists' do
       expect(@content_service).to receive(:create_issue) do |args|
         expect(args[:text]).to include("#[Solution]#\n\nApache HTTPD >= 2.0 and < 2.0.65")
         OpenStruct.new(args)
@@ -157,14 +157,14 @@ describe 'Nexpose upload plugin' do
 
       expect(@content_service).to receive(:create_issue) do |args|
         expect(args[:text]).to include("#[Solution]#\n")
-        expect(args[:text]).to include("You can remove inode information from the ETag header")
+        expect(args[:text]).to include('You can remove inode information from the ETag header')
         OpenStruct.new(args)
       end.once
 
       @importer.import(file: 'spec/fixtures/files/full.xml')
     end
 
-    it "populates tests regardless of if they are wrapped in paragraphs or containerblockelements" do
+    it 'populates tests regardless of if they are wrapped in paragraphs or containerblockelements' do
       expect(@content_service).to receive(:create_evidence) do |args|
         expect(args[:content]).to include("#[Content]#\nThe following NTP variables")
         OpenStruct.new(args)
@@ -178,7 +178,7 @@ describe 'Nexpose upload plugin' do
       @importer.import(file: '/spec/fixtures/files/full.xml')
     end
 
-    it "transforms html entities (&lt; and &gt;)" do
+    it 'transforms html entities (&lt; and &gt;)' do
       expect(@content_service).to receive(:create_issue) do |args|
         expect(args[:text]).to include("#[Solution]#\n\nApache HTTPD >= 2.0 and < 2.0.65")
         OpenStruct.new(args)
