b68e54a4c302c8add885c87f1ffa794a0a447330
Caitlin <caitlinhenry.ss@gmail.com>
2023-11-30 16:49:44 -0500

    revert gem version bump

--- lib/dradis/plugins/nexpose/gem_version.rb
+++ lib/dradis/plugins/nexpose/gem_version.rb
@@ -9,7 +9,7 @@ module Dradis
       module VERSION
         MAJOR = 4
         MINOR = 10
-        TINY = 1
+        TINY = 0
         PRE = nil
 
         STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
