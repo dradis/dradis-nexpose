7dac5784ffb628f33df6419fab3f2a403a7e9de7
Caitlin <caitlinhenry.ss@gmail.com>
2024-03-20 10:25:51 -0400

    require mapping module

--- lib/dradis/plugins/nexpose.rb
+++ lib/dradis/plugins/nexpose.rb
@@ -8,4 +8,5 @@ end
 require 'dradis/plugins/nexpose/engine'
 require 'dradis/plugins/nexpose/field_processor'
 require 'dradis/plugins/nexpose/importer'
+require 'dradis/plugins/nexpose/mapping'
 require 'dradis/plugins/nexpose/version'
