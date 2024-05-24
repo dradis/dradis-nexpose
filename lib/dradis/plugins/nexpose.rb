8e343b59e6180ef9774e30c1155609481539ddae
Aaron Manaloto <mail@aapomm.io>
2024-05-24 14:50:21 +0800

    Use a more general XML Formatter

--- lib/dradis/plugins/nexpose.rb
+++ lib/dradis/plugins/nexpose.rb
@@ -8,6 +8,6 @@ end
 require 'dradis/plugins/nexpose/engine'
 require 'dradis/plugins/nexpose/field_processor'
 require 'dradis/plugins/nexpose/importer'
-require 'dradis/plugins/nexpose/list_formatter'
 require 'dradis/plugins/nexpose/mapping'
 require 'dradis/plugins/nexpose/version'
+require 'dradis/plugins/nexpose/xml_formatter'
--- spec/xml_formatter_spec.rb
+++ spec/xml_formatter_spec.rb
@@ -1,12 +1,12 @@
-# Run the spec by running the command: rspec spec/list_formatter_spec.rb"
+# Run the spec by running the command: rspec spec/xml_formatter_spec.rb"
 
 require 'spec_helper'
 
-describe Dradis::Plugins::Nexpose::ListFormatter do
+describe Dradis::Plugins::Nexpose::XmlFormatter do
   let(:source) { File.read(File.expand_path('../fixtures/files/lists.xml', __FILE__)) }
 
   it 'parses the <UnorderedList> and <OrderedList> elements' do
-    expect(Dradis::Plugins::Nexpose::ListFormatter.new.format_list(source)).to eq(
+    expect(Dradis::Plugins::Nexpose::XmlFormatter.new.format_list(source)).to eq(
       "* Microsoft Windows\n"\
       "## Open the Windows Control Panel.\n"\
       "## Select \"Administrative Tools\".\n"\
