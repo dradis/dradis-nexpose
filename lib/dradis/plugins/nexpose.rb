f84272124b77d579bbdd8ab1bb545d77f6981397
Caitlin <caitlinhenry.ss@gmail.com>
2024-09-04 16:12:11 -0400

    separate full & simple formatters into importers

--- lib/dradis/plugins/nexpose.rb
+++ lib/dradis/plugins/nexpose.rb
@@ -7,6 +7,7 @@ end
 
 require 'dradis/plugins/nexpose/engine'
 require 'dradis/plugins/nexpose/field_processor'
-require 'dradis/plugins/nexpose/importer'
+require 'dradis/plugins/nexpose/full/importer'
 require 'dradis/plugins/nexpose/mapping'
+require 'dradis/plugins/nexpose/simple/importer'
 require 'dradis/plugins/nexpose/version'
